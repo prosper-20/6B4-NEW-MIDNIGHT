4852ebd8d91e23d5e2887b2e8b591d24180b0e6c
prosper <edwardprosper001@gmail.com>
2023-08-31 02:48:05 +0100

    'updates'

--- NEWS/settings.py
+++ NEWS/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['my6b4news-ee2701c086fa.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
